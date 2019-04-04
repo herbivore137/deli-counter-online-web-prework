a6ff940cc8b8afa526383d6deaa212d4c23dc1b1
B.A. Hanlon <herbivore137@gmail.com>
2019-04-04 17:23:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 katz_deli = []
 
 def line(arr)
+=begin
   if arr.size != 0 
     counter = 1
     str = "The line is currently:"
@@ -12,6 +13,20 @@ def line(arr)
   else 
     puts "The line is currently empty."
   end
+=end
+
+if arr.size != 0 
+    counter = 1
+    str = "The line is currently:"
+    arr.each do |x|
+      str << " #{counter}. #{x}"
+      counter += 1
+    end
+    puts str
+  else 
+    puts "The line is currently empty."
+  end
+
 end
 
 def take_a_number(arr, name)
