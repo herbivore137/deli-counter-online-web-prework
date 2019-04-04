d2471a02d43d43b354e0569a377028b8adf38a81
B.A. Hanlon <herbivore137@gmail.com>
2019-04-04 17:09:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,14 @@ katz_deli = []
 
 def line(arr)
   
-  if arr.length != 0 
+  if arr.size != 0 
     counter = 1
     str = "The line is currently: "
     arr.each do |x|
       str << "#{counter}. #{x} "
       counter += 1
     end
-    str
+    puts str
   else 
     puts "The line is currently empty."
   end
