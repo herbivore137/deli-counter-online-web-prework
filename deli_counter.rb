9d75ae664124bde8dfb64601d616a492406195b6
B.A. Hanlon <herbivore137@gmail.com>
2019-04-04 17:10:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(arr)
   
   if arr.size != 0 
     counter = 1
-    str = "The line is currently: "
+    str = "The line is currently:"
     arr.each do |x|
-      str << "#{counter}. #{x} "
+      str << " #{counter}. #{x}"
       counter += 1
     end
     puts str
